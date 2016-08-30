8f69a237da7bfcf321a8f299a367988cdad9fc43
Oleksandr Iegorov <oiegorov@magento.com>
2016-08-30 10:57:52 +0300

    MAGETWO-57678: Do post-install tweaks

--- install.sh
+++ install.sh
@@ -44,3 +44,4 @@ docker-compose up --build -d
 
 docker exec -it --privileged magento2-devbox-web php /root/scripts/composerInstall.php
 docker exec -it --privileged magento2-devbox-web php /root/scripts/magentoSetup.php
+docker exec -it --privileged magento2-devbox-web /bin/bash /root/scripts/postInstall.sh
