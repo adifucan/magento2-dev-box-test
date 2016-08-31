f3b3d0b78e5d84dcd94df9c4047b9cd81660137b
Oleksandr Iegorov <oiegorov@magento.com>
2016-08-31 16:22:22 +0300

    MAGETWO-57680: Improve existing install script

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -1,5 +1,5 @@
 cd /var/www/magento2
-#php bin/magento deploy:mode:set developer
+php bin/magento deploy:mode:set developer
 #php bin/magento setup:static-content:deploy
 #php bin/magento setup:di:compile
 echo "* * * * * root /usr/local/bin/php /var/www/magento2/bin/magento cron:run | grep -v \"Ran jobs by schedule\" >> /var/www/magento2/var/log/magento.cron.log" >> /etc/crontab
