baa31a272adcdeeb08ddf231284eb620ac74aa71
Dmytro Poperechnyy <adifucan@gmail.com>
2016-08-31 22:08:12 +0300

    MAGETWO-57690: Investigate and document how to debug w. xDebug

--- scripts/postInstall.php
+++ scripts/postInstall.php
@@ -1,7 +1,7 @@
 <?php
 passthru('cd /var/www/magento2 && php bin/magento deploy:mode:set developer');
-//passthru('cd /var/www/magento2 &&php bin/magento setup:static-content:deploy');
-//passthru('cd /var/www/magento2 && php bin/magento setup:di:compile');
+passthru('cd /var/www/magento2 &&php bin/magento setup:static-content:deploy');
+passthru('cd /var/www/magento2 && php bin/magento setup:di:compile');
 shell_exec('echo "* * * * * root /usr/local/bin/php /var/www/magento2/bin/magento cron:run | grep -v \"Ran jobs by schedule\" >> /var/www/magento2/var/log/magento.cron.log" >> /etc/crontab');
 shell_exec('echo "* * * * * root /usr/local/bin/php /var/www/magento2/update/cron.php >> /var/www/magento2/var/log/update.cron.log" >> /etc/crontab');
 shell_exec('echo "* * * * * root /usr/local/bin/php /var/www/magento2/bin/magento setup:cron:run >> /var/www/magento2/var/log/setup.cron.log" >> /etc/crontab');
