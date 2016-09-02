cf118f4efa85f4084598cbbd57d870e228ec2e22
Dmytro Poperechnyy <dpoperechnyy@magento.com>
2016-09-02 17:47:35 +0300

    MAGETWO-57666: Investigate and setup Grunt/Gulp

--- web/scripts/postInstall.php
+++ web/scripts/postInstall.php
@@ -1,6 +1,34 @@
 <?php
+
+function readValue($defaultValue = null)
+{
+    $input = rtrim(fgets(STDIN));
+    return $input ?: $defaultValue;
+}
+
 passthru('cd /var/www/magento2 && php bin/magento deploy:mode:set developer');
-passthru('cd /var/www/magento2 &&php bin/magento setup:static-content:deploy');
+
+$response = true;
+while ($response) {
+    echo 'Do you want to pre-deploy all static assets (d) or just compile CSS out of LESS via Grunt (g) or none (N)? d/g/N: ';
+    $deployStatic = readValue();
+    $response = false;
+    switch ($deployStatic) {
+        case null:
+        case 'N':
+            return;
+        case 'd':
+            passthru('cd /var/www/magento2 && php bin/magento -f setup:static-content:deploy');
+            break;
+        case 'g':
+            passthru('cd /var/www/magento2 && cp Gruntfile.js.sample Gruntfile.js && cp package.json.sample package.json');
+            passthru('cd /var/www/magento2 && npm install && grunt refresh');
+            break;
+        default:
+            $response = true;
+    }
+}
+
 passthru('cd /var/www/magento2 && php bin/magento setup:di:compile');
 shell_exec('echo "* * * * * root /usr/local/bin/php /var/www/magento2/bin/magento cron:run | grep -v \"Ran jobs by schedule\" >> /var/www/magento2/var/log/magento.cron.log" >> /etc/crontab');
 shell_exec('echo "* * * * * root /usr/local/bin/php /var/www/magento2/update/cron.php >> /var/www/magento2/var/log/update.cron.log" >> /etc/crontab');
