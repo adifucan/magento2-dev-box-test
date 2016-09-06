aeda529f400a3e87cf8791f16623bbaf81879cae
Viktor Paladiychuk <vpaladiychuk@magento.com>
2016-09-06 17:16:39 +0300

    MAGETWO-57665: Modify Docker compose file [redis]

--- web/scripts/setupRedis.php
+++ web/scripts/setupRedis.php
@@ -4,7 +4,9 @@ $options = getopt(
     '',
     [
         'redis-host::',
-        'magento-path::'
+        'magento-path::',
+        'as-cache',
+        'as-session'
     ]
 );
 
@@ -13,33 +15,37 @@ $redisHost = !empty($options['redis-host']) ? $options['redis-host'] : 'redis';
 
 $conf = include $magentoPath . '/app/etc/env.php';
 
-$conf['session'] = [
-    'save' => 'redis',
-    'redis' =>
-        [
-            'host' => $redisHost,
-            'port' => '6379',
-            'password' => '',
-            'timeout' => '2.5',
-            'persistent_identifier' => '',
-            'database' => '0',
-            'compression_threshold' => '2048',
-            'compression_library' => 'gzip',
-            'log_level' => '1',
-            'max_concurrency' => '6',
-            'break_after_frontend' => '5',
-            'break_after_adminhtml' => '30',
-            'first_lifetime' => '600',
-            'bot_first_lifetime' => '60',
-            'bot_lifetime' => '7200',
-            'disable_locking' => '0',
-            'min_lifetime' => '60',
-            'max_lifetime' => '2592000'
-        ]
-];
+if (!empty($options['as-session'])) {
+    $conf['session'] = [
+        'save' => 'redis',
+        'redis' =>
+            [
+                'host' => $redisHost,
+                'port' => '6379',
+                'password' => '',
+                'timeout' => '2.5',
+                'persistent_identifier' => '',
+                'database' => '0',
+                'compression_threshold' => '2048',
+                'compression_library' => 'gzip',
+                'log_level' => '1',
+                'max_concurrency' => '6',
+                'break_after_frontend' => '5',
+                'break_after_adminhtml' => '30',
+                'first_lifetime' => '600',
+                'bot_first_lifetime' => '60',
+                'bot_lifetime' => '7200',
+                'disable_locking' => '0',
+                'min_lifetime' => '60',
+                'max_lifetime' => '2592000'
+            ]
+    ];
+} else {
+    $conf['session'] = ['save' => 'files'];
+}
 
-$conf['cache']['frontend'] =
-    [
+if (!empty($options['as-cache'])) {
+    $conf['cache']['frontend'] = [
         'default' =>
             [
                 'backend' => 'Cm_Cache_Backend_Redis',
@@ -61,5 +67,8 @@ $conf['cache']['frontend'] =
                     ]
             ]
     ];
+} else {
+    unset($conf['cache']['frontend']);
+}
 
 file_put_contents($magentoPath . '/app/etc/env.php', "<?php\n return " . var_export($conf, true) . ';');
